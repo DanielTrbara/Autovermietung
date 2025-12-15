eb21f6359da1f6133ee56a9095168ca9f4cb5f19
quantumsine <thelinn.sp@gmail.com>
2025-12-15 19:57:16 +0100

    Changes in der Maintenence-View, DB Connection zur Wartung

--- src/main/java/org/example/autovermietung/Launcher.java
+++ src/main/java/org/example/autovermietung/Launcher.java
@@ -1,9 +1,13 @@
 package org.example.autovermietung;
 
+import org.example.autovermietung.Databases.MaintenanceDB;
 import org.example.autovermietung.Model.Dashboard;
 
+import java.sql.SQLException;
+
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        MaintenanceDB.initDB();
         Dashboard.main(args);
     }
 }
